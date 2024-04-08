e3a78f0667b5f3cd78a7e41c2976d91c1c7c6bd4
takanbi <tunji.akanbi@century.edu>
2024-04-08 12:56:20 -0500

    added more gasp plugins

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+// gsap.registerPlugin(GSDevTools, SplitText, TextPlugin, ScrollTrigger, DrawSVGPlugin);
 gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(SplitText);
+// gsap.registerPlugin(GSDevTools, TextPlugin, SplitText);
+// gsap.registerPlugin(GSDevTools, TextPlugin, SplitText);
+// gsap.registerPlugin(GSDevTools, TextPlugin, SplitText);
 $(function () {
-  GSDevTools.create();
+  // GSDevTools.create();
   let content = document.getElementById("content");
   let demoBox = document.getElementById("demoBox");
   let titleSpans = document.querySelectorAll("#content > h1 > span");
@@ -37,6 +43,10 @@ $(function () {
   }
 
   function Step004() {
+    // gsap.set(para, {
+    //   autoAlpha: 0,
+    //   y: 300
+    // })
     let tl = gsap.timeline({
       id: "Step004",
     });
@@ -72,4 +82,97 @@ $(function () {
   $("section.monserrat").on("mouseenter", () => {
     this.css("transform", "scale(2)");
   });
+//text plugin
+gsap.to("p#one", {
+  // text: {
+  //   value: "This is some text",
+  //   delimiter: " "
+  // }
+  text: "typewriter effect with GSAP 3",
+  ease: "power1.in",
+  duration: 2,
+  repeat: 10,
+  yoyo: true,
+  repeatDelay: 0.4
+})
+
+//split text letters
+let split1;
+let animation1 = gsap.timeline({
+  id: "animation1",
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 0.3
+})
+function init1() {
+  gsap.set("#container3", {
+    autoAlpha: 1
+  })
+  split1 = new SplitText("p#two", {
+    type: "chars"
+  })
+  animation1.from(split1.chars, {
+    opacity: 0,
+    y: 50,
+    ease: "back(4)",
+    stagger: 0.05
+  })
+  // GSDevTools.create({
+  //   animation1: "animation1"
+  // })
+}
+window.addEventListener("load", init1)
+//split text words
+let split2;
+let animation2 = gsap.timeline({
+  id: "animation2"
+})
+function init2() {
+  split2 = new SplitText("p#three", {
+    type: "words"
+  })
+  gsap.set("#container4", {
+    autoAlpha: 1
+  })
+  animation2.from(split2.words, {
+    duration: 3,
+    opacity: 0,
+    rotation: -40,
+    scale: 0.4,
+    stagger: 0.1,
+    ease: "back",
+    repeat: -1
+  })
+  // GSDevTools.create({
+  //   animation2: "animation2"
+  // })
+}
+window.addEventListener("load", init2)
+//split text line
+let split3;
+let animation3 = gsap.timeline({
+  id: "animation3",
+  repeat: -1,
+  repeatDelay: 3
+})
+function init3() {
+  split3 = new SplitText("p#four", {
+    type: "lines"
+  })
+  gsap.set("#container5", {
+    autoAlpha: 1
+  })
+  animation3.from(split3.lines, {
+    opacity: 0,
+    rotationX: -90,
+    rotationY: -80,
+    stagger: 0.4,
+    transformOrigin: "50% 50% -200"
+  })
+  // GSDevTools.create({
+  //   animation3: "animation3"
+  // })
+}
+window.addEventListener("load", init3)
+//jQuery ends here
 });
